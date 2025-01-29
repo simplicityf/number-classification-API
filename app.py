be93c2f6ac0b204f0b6cc05c5a9aa47ab7373ca2
simplicityf <omobolanlehazeezat@gmail.com>
2025-01-29 17:33:47 +0100

    Just a small change at the app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ CORS(app)
 
 @app.route('/')
 def home():
+    
     # Return JSON response containing the GitHub URL, current datetime, and email
-
     return jsonify(email="omobolanlehazeezat@gmail.com", current_datetime=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"), GitHub_url="https://github.com/simplicityf/hng_task1.git")
 
 
