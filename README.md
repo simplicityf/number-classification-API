f467b96b9ca94222905a96ccdffa263a651cc24e
simplicityf <omobolanlehazeezat@gmail.com>
2025-01-29 17:14:09 +0100

    Create webpage that returns a json format
    The json response contain an email, current_time and github_url

---
+++ README.md
@@ -0,0 +1,19 @@
+# This is a simple Flask application returning a json format
+
+# Getting Started
+
+Set up virtual environment (Note: This is on windows)
+```
+python -m venv venv
+venv/Scripts/activate
+``` 
+
+To install the dependencies
+```
+pip install -r requirements.txt
+```
+
+To run the app
+```
+flask --app app run
+```
--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import Flask, jsonify
 import datetime
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def home():
-    return jsonify(email="omobolanlehazeezat@gmail.com", date=datetime)
+    # Return JSON response containing the GitHub URL, current datetime, and email
+
+    return jsonify(email="omobolanlehazeezat@gmail.com", current_datetime=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%SZ"), GitHub_url="https://github.com/simplicityf/hng_task1.git")
+
+
+if __name__ == '__main__':
+    app.run()
